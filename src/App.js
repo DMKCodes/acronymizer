40c248ed0b0b24e0f852ba411345ae3b605161a0
Doug <dmkcodes@gmail.com>
2023-03-31 14:27:38 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { words } from './WORDS';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [acronym, setAcronym] = useState([]);
+
+    const generateAcronym = () => {
+        const newAcronym = [];
+        for (const letter in words) {
+            const wordsList = words[letter];
+            const index = Math.floor(Math.random() * words.length);
+            newAcronym.push(wordsList[index]);
+        }
+        setAcronym(newAcronym);
+    };
+
+    useEffect(() => {
+        generateAcronym();
+    }, []);
+
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
